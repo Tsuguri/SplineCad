e8b8bbd58a18b0ee3584679dc5f001672d20ec31
Fnyt <fnytyoyo@gmail.com>
2017-05-25 22:47:47 +0200

    file manager class

---
+++ SplineCAD/Utilities/FileManager.cs
@@ -0,0 +1,18 @@
+﻿using SplineCAD.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplineCAD.Utilities
+{
+    public static class FileManager
+    {
+        public static void ExportToIGS(List<Model> models)
+        {
+
+        }
+
+    }
+}
